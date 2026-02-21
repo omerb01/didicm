0d1d8a2d57f834a046122378fa50866383fddaf4
Omer Belhasin <obelhasin@nvidia.com>
2026-02-21 12:03:57 -0800

    Fix repo installation

--- setup.py
+++ setup.py
@@ -8,11 +8,15 @@ from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
+# Install dependencies from requirements.txt
 requirements = []
 requirements_file = this_directory / "requirements.txt"
 if requirements_file.exists():
-    requirements = requirements_file.read_text(encoding="utf-8").strip().split("\n")
-    requirements = [req.strip() for req in requirements if req.strip() and not req.startswith("#")]
+    requirements = [
+        req.strip()
+        for req in requirements_file.read_text(encoding="utf-8").strip().split("\n")
+        if req.strip() and not req.startswith("#")
+    ]
 
 setup(
     name="didicm",
@@ -23,14 +27,14 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/omerb01/didicm",
-    packages=find_packages(where="didicm"),
+    packages=find_packages(),
+    license="MIT",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Science/Research",
         "Intended Audience :: Developers",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
         "Topic :: Scientific/Engineering :: Image Recognition",
-        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
